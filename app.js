b606feabc96b673cd7fefab72f9e644454ef9af1
Jithmi Chathurangika <162092982+Jithmi01@users.noreply.github.com>
2024-12-11 16:50:20 +0530

    new

--- app.js
+++ app.js
@@ -2,43 +2,61 @@
 
 const http = require('http');
 require('dotenv').config()
+const express = require ('express');
+const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT;
 
+const app = express();
+
+
 let customerList=[
-    {id:1,name:'Kamal'},
-    {id:2,name:'Saman'},
+    {id:1001,name:'Kamal'},
+    {id:1002,name:'Saman'},
 
 ];
 
-const server = http.createServer((req,res)=>{
+// parse application
+app.use(bodyParser.urlencoded({extended:false}))
 
-    // Load all customers (GET)
-    if(req.method=='GET' && req.url==='/get-customers'){ //http://localhost:3000/get-customers
-        res.writeHead(200,{'Content-Type':'application/json'});
-        res.end(JSON.stringify(customerList));
+// parser application/json
+app.use(bodyParser.json())
 
-    }else if(req.method=='POST' && req.url==='/add-customer'){
-        // Add new customer(POST)
-        res.writeHead(200,{'Content-Type':'application/json'});
-        res.end('Customer saved');
 
-    }else if(req.method=='PUT' && req.url==='/update-customer'){
-        //  Update customer(PUT)
-        res.writeHead(200,{'Content-Type':'application/json'});
-        res.end('Customer updated');
 
-    }else if(req.method=='DELETE' && req.url==='/delete-customer'){
-        // Delete customer(DELETE)
-        res.writeHead(200,{'Content-Type':'application/json'});
-        res.end('Customer deleted');
-        
-    }else{
-        res.writeHead(404,{'Content-Type':'text/plain'});
-        res.end('Route Not Found')
-    }
+// Create a customer
+app.post('/create', (req, resp) => {
+    console.log(req.body);
+    resp.status(201).json({message: 'Customer saved'});
+});
+
+// Find all customers
+app.get('/find-all', (req, resp) => {
+    resp.status(200).json({message: 'Customer list..', dataList: [{name: 'kamal'}, {name: 'Jagath'}], count: 1500});
+});
+
+// Find customer by id
+app.get('/find-by-id/:id', (req, resp) => {
+    resp.status(200).json({message: 'Selected Customer', data: {name:'kamal'}});
+});
+
+// Update customer by id
+app.put('/update-by-id/:id', (req, resp) => {
+    console.log(req.body);
+    resp.status(201).json({message: 'Customer updated'});
+});
+
+// Update customer by id
+app.delete('/delete-by-id/:id', (req, resp) => {
+    resp.status(201).json({message: 'Customer deleted'});
 });
 
 
-server.listen(PORT,()=>{
-    console.log(`Server started & running on port ${PORT}`);
-});
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server started & running on ${PORT}`);
+})
